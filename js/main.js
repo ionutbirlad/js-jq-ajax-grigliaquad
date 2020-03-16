e47ba64e5c61f9e719c9246e90df922a464f6e26
Ionut Birlad <birladionut@yahoo.it>
2020-03-16 14:00:13 +0100

    avanzamento

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 $(document).ready(function() {
 
-  
+  $(document).on("click", ".quadratino", function() {
+
+    $.ajax({
+      url: "https://flynn.boolean.careers/exercises/api/random/int",
+      method: "GET",
+      success: function(data) {
+        // console.log(data.response);
+      },
+      error: function() {
+        alert("C'è stato un errore!")
+      }
 
 
 
+    });
 
 
+  });
 
 
 
