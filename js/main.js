e8f064bf8381d752cb5c252bba91a6581f5a6f12
Ionut Birlad <birladionut@yahoo.it>
2020-03-16 14:06:14 +0100

    esercizio fatto con ripetizione nell'HTML

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 $(document).ready(function() {
 
   $(document).on("click", ".quadratino", function() {
-
+    var myThis = $(this);
     $.ajax({
       url: "https://flynn.boolean.careers/exercises/api/random/int",
       method: "GET",
       success: function(data) {
         // console.log(data.response);
+        if (data.response <= 5) {
+          $(myThis).text(data.response);
+          $(myThis).addClass("giallo");
+        } else {
+          $(myThis).text(data.response);
+          $(myThis).addClass("rosso");
+        }
       },
       error: function() {
         alert("C'è stato un errore!")
       }
-
-
-
     });
 
 
